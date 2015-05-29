b91ce4bbedf2021f42ca598333b602d008445bc6
Akif Yanbak <makifyanbak@gmail.com>
2015-05-29 14:50:39 +0300

    OnStop metotu eklendi

--- MailWinService/MailWinService/MailService.cs
+++ MailWinService/MailWinService/MailService.cs
@@ -20,17 +20,25 @@ namespace MailWinService
                 /* Ilk parametre ile, "Log Test" ismi altinda tutulacak 
                  * Log bilgilerinin kaynak ismi belirleniyor. Daha sonra 
                  * bu kaynak ismi _eventLog isimli nesnemizin Source özelligine ataniyor.*/
-                EventLog.CreateEventSource("SampleSource", "Log Test");
+                EventLog.CreateEventSource("SampleSource", "Log");
             }
             _eventLog.Source = "SampleSource";
 
             /* Log olarak ilk parametrede belirtilen mesaj yazilir. 
              * Log'un tipi ise ikinci parametrede görüldügü gibi Information'dir.*/
-            _eventLog.WriteEntry("Our service was launched...", EventLogEntryType.Information);
+            _eventLog.WriteEntry("Our service was started.", EventLogEntryType.Information);
         }
 
         protected override void OnStop()
         {
+            _eventLog = new EventLog();
+            if (!EventLog.SourceExists("SampleSource"))
+            {
+                EventLog.CreateEventSource("SampleSource", "Log");
+            }
+            _eventLog.Source = "SampleSource";
+            _eventLog.WriteEntry("Our service was stopped.");
+
         }
     }
 }
